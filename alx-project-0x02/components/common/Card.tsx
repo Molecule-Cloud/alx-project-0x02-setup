d41ea804e6050b2aeb039d2e38885d2292eab32a
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-23 16:37:29 +0000

    Added acrd props interface

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,6 @@
 import { CardProps } from '@/interfaces'
 
-const Card: React.FC <CardProps>= ({ title, content }) => {
+const Card: React.FC = ({ title, content }: CardProps) => {
     return (
         <>
             <div className='flex items-center flex-col p-12  border'>
