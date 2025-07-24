6c1127ce8ba1ab0f992f4f469c4a2d7255cc2d5b
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-24 12:34:35 +0000

    Added posts page

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
                             <Link href="/about"></Link>
                         </li>
                         <li>Explore
-                            <Link href="posts"></Link>
+                            <Link href="/posts"></Link>
                         </li>
                     </ul>
                 </nav>
